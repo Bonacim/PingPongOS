f27bef4edb55b366bb316481c2d0315f9744a4f4
Bonacim <bonacim@alunos.utfpr.edu.br>
2020-10-08 14:14:17 -0300

    p06

---
+++ p06/datatypes.h
@@ -0,0 +1,53 @@
+// PingPongOS - PingPong Operating System
+// Prof. Carlos A. Maziero, DAINF UTFPR
+// Versão 1.0 -- Março de 2015
+//
+// Estruturas de dados internas do sistema operacional
+
+#ifndef __DATATYPES__
+#define __DATATYPES__
+#include <ucontext.h>
+
+typedef enum {pronta, suspensa} status_t;
+
+// Estrutura que define uma tarefa
+typedef struct task_t
+{
+    struct task_t *prev, *next;
+    int tid;
+    ucontext_t context;
+    status_t status;
+    struct task_t *wqueue;
+    int prio_s;
+    int prio_d;
+    int ticks_t;
+    unsigned int created_at;
+    unsigned int proc_time;
+    unsigned int activations;
+} task_t ;
+
+// estrutura que define um semáforo
+typedef struct
+{
+  // preencher quando necessário
+} semaphore_t ;
+
+// estrutura que define um mutex
+typedef struct
+{
+  // preencher quando necessário
+} mutex_t ;
+
+// estrutura que define uma barreira
+typedef struct
+{
+  // preencher quando necessário
+} barrier_t ;
+
+// estrutura que define uma fila de mensagens
+typedef struct
+{
+  // preencher quando necessário
+} mqueue_t ;
+
+#endif
